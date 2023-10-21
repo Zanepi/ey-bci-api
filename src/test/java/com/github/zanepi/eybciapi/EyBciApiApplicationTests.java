a8d79c11cc40e60bd75fcd34e7f59f4269335cbe
Brian Galdames <brian@groovelist.co>
2023-10-20 21:13:53 -0300

    First Commit

---
+++ src/test/java/com/github/zanepi/eybciapi/EyBciApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.zanepi.eybciapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EyBciApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
